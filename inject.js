9838a34838a3de6e8f4e596c56d1597ac491a0f4
kdongha <kdongha1597@gmail.com>
2019-09-04 22:26:10 +0900

    create boj/swea/jungol.js

--- inject.js
+++ inject.js
@@ -5,4 +5,15 @@ function injectScript(file_path, tag) {
     script.setAttribute('src', file_path);
     node.appendChild(script);
 }
-injectScript(chrome.extension.getURL('content.js'), 'body');
+const rexBoj = new RegExp("https://www.acmicpc.net/submit/")
+const rexSwea = new RegExp("https://swexpertacademy.com/main/solvingProblem/solvingProblem.do")
+const rexJungol = new RegExp("http://www.jungol.co.kr/theme/jungol/submitpage.php?")
+
+let url = window.location.href
+if (rexBoj.test(url)) {
+    injectScript(chrome.extension.getURL('boj.js'), 'body');
+} else if (rexSwea.test(url)) {
+    injectScript(chrome.extension.getURL('swea.js'), 'body');
+} else if (rexJungol.test(url)) {
+    injectScript(chrome.extension.getURL('jungol.js'), 'body');
+}
---
+++ swea.js
@@ -0,0 +1,56 @@
+let editor = document.querySelector('.CodeMirror')
+editor.addEventListener('paste', function () {
+    let lang = document.querySelector('#sel_lang').value
+    if (lang === 'J') {
+        let codemirror = editor.CodeMirror
+        codemirror.setValue(changeCode(codemirror.getValue(), "Solution"))
+    }
+})
+
+function changeCode(code, name) {
+    let rexPackage = new RegExp('package')
+    let rexClass = new RegExp('class .*')
+    let rexPsvm = new RegExp('public static void main')
+    let classStack = []
+    let lines = code.trim().split('\n')
+    if (rexPackage.test(lines[0])) {
+        lines = lines.slice(1, lines.length)
+    }
+    for (let i = 0; i < lines.length; i++) {
+        if (rexPsvm.test(lines[i])) {
+            let line = classStack[0].line
+            while (!rexClass.test(lines[line])) {
+                line--;
+            }
+            let checkClass = false
+            let arr = lines[line].split(' ')
+            for (let idx = 0; idx < arr.length; idx++) {
+                if (checkClass) {
+                    arr[idx] = name
+                    break
+                } else if (arr[idx] === 'class') {
+                    checkClass = true
+                }
+            }
+            lines[line] = arr.join(' ')
+            break;
+        }
+        let chars = lines[i].split('')
+        for (let char of chars) {
+            if (char === '{') {
+                if (classStack.length === 0) {
+                    classStack.push({ 'line': i, 'count': 1 })
+                } else {
+                    classStack[classStack.length - 1].count++
+                }
+            } else if (char === '}') {
+                if (classStack[classStack.length - 1].count == 1) {
+                    classStack.pop()
+                } else {
+                    classStack[classStack.length - 1].count--
+                }
+            }
+        }
+    }
+    return lines.join('\n').trim()
+}
